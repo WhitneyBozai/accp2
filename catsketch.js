b282f4009bac215ee3cb454e33ac39a278f66db2
WhitneyBozai <874451418@qq.com>
2025-04-04 20:12:14 -0400

    Update catsketch.js

--- catsketch.js
+++ catsketch.js
@@ -9,9 +9,9 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth,windowHeight);
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(windowWidth,windowHeight);
   video.hide();
   
   faceMesh.detectStart(video, gotFaces);
